0f90023a92d0dc42f14d524c8b8156136e3eabf9
verdantkyber <kingjalexa@gmail.com>
2018-10-02 22:47:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(cat){
+  kittens.push(cat);
+}
+
+function destructivelyPrependKitten(cat){
+  kittens.unshift(cat);
+}
+
+function destructivelyRemoveLastKitten(cat){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(cat){
+  kittens.shift();
+}
+
+function appendKitten(cat){
+  kittens = kittens.concat(cat);
+  return kittens;
+}
